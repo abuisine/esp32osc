5f2c292ccb8d4fa381af3a35c81e09fd1443ca47
Alexandre Buisine <alexandre.buisine@enix.fr>
2022-03-14 23:12:02 +0100

    feat: read preferences and render them on webpage

--- esp32osc.ino
+++ esp32osc.ino
@@ -5,13 +5,26 @@
 #include <ETH.h>
 #include <SPIFFS.h>
 #include <OSCMessage.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
 WiFiUDP udp;
+AsyncWebServer server(80);
 
 static bool eth_connected = false;
-const int inPort = 3333;
-IPAddress outIp(192, 168, 1, 26);
-const unsigned int outPort = 9999;
+
+static uint16_t inPort;
+static uint16_t outPort;
+static IPAddress outHost;
+static String oscAddress;
+#define SETTINGS_DEFAULT_IN_PORT 9998
+#define SETTINGS_DEFAULT_OUT_PORT 9999
+#define SETTINGS_DEFAULT_OUT_HOST IPAddress(192, 168, 1, 28)
+#define SETTINGS_DEFAULT_OSC_ADDRESS String("esp32osc")
+
+#include <Preferences.h>
+Preferences prefs;
+
 
 void WiFiEvent(WiFiEvent_t event)
 {
@@ -36,7 +49,7 @@ void WiFiEvent(WiFiEvent_t event)
       Serial.print(ETH.linkSpeed());
       Serial.println("Mbps");
       eth_connected = true;
-      udp.begin(WiFi.localIP(), inPort);
+//      udp.begin(WiFi.localIP(), inPort);
 
       break;
     case ARDUINO_EVENT_ETH_DISCONNECTED:
@@ -56,33 +69,62 @@ void WiFiEvent(WiFiEvent_t event)
 
 #define BUTTON_PRESSED()  (!digitalRead (34))
 
+String processor(const String& var)
+{
+  if(var == "MAC_ADDRESS")
+    return ETH.macAddress();
+  if(var == "IPV4")
+    return ETH.localIP().toString();
+  if(var == "LINK_SPEED")
+    return String(ETH.linkSpeed());
+
+  if(var == "IN_PORT")
+    return String(inPort); 
+  if(var == "OUT_PORT")
+    return String(outPort);
+  if(var == "OUT_HOST_0")
+    return String(outHost[0]);
+  if(var == "OUT_HOST_1")
+    return String(outHost[1]);
+  if(var == "OUT_HOST_2")
+    return String(outHost[2]);
+  if(var == "OUT_HOST_3")
+    return String(outHost[3]);
+  if(var == "OSC_ADDRESS")
+    return oscAddress;
+  return String();
+}
+
 void setup()
 {
   Serial.begin(115200);
-  delay(2000);
+  Serial.println("esp32osc starting ...");
+
+  prefs.begin("settings", true);
+  inPort = prefs.getUInt("inPort", SETTINGS_DEFAULT_IN_PORT);
+  outPort = prefs.getUInt("outPort", SETTINGS_DEFAULT_OUT_PORT);
+  outHost = prefs.getULong("outHost", SETTINGS_DEFAULT_OUT_HOST);
+  oscAddress = prefs.getString("oscAddress", SETTINGS_DEFAULT_OSC_ADDRESS);
+
+  Serial.print("inPort: "); Serial.println(inPort);
+  Serial.print("outPort: "); Serial.println(outPort);
+  Serial.print("outHost: "); Serial.println(outHost);
+  Serial.print("oscAddress: "); Serial.println(oscAddress);
 
   WiFi.onEvent(WiFiEvent);
   ETH.begin();
   pinMode (34, INPUT);  // Button
 
-
   if(!SPIFFS.begin(true)){
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
-  
-  File file = SPIFFS.open("/index.html");
-  if(!file){
-    Serial.println("Failed to open file for reading");
-    return;
-  }
-  
-  Serial.println("File Content:");
-  while(file.available()){
-    Serial.write(file.read());
-  }
-  file.close();
-  
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+  server.begin();
 }
 
 
@@ -94,14 +136,14 @@ void loop()
     Serial.println("ETH Status:");
     Serial.println(ETH.linkUp());
 
-    OSCMessage msg("/address");
-    msg.add(64);
-
-    udp.beginPacket(outIp, outPort);
-    msg.send(udp); // send the bytes to the SLIP stream
-    udp.endPacket();
-
-    msg.empty();
+//    OSCMessage msg("/address");
+//    msg.add(64);
+//
+//    udp.beginPacket(outIp, outPort);
+//    msg.send(udp); // send the bytes to the SLIP stream
+//    udp.endPacket();
+//
+//    msg.empty();
     while (BUTTON_PRESSED());
   }
   delay(2000);
